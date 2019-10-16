95390b20a0541206853f18d2a4f936916fdf9071
Kirilllert <46005727+Kirilllert@users.noreply.github.com>
2019-10-17 00:38:52 +0300

    add class gain

--- filters.js
+++ filters.js
@@ -1,7 +1,16 @@
-const gainNode = context.createGain();
-gainNode.gain.value = 0.4;
+class Gains {
+  createNode() {
+    const gainNode = context.createGain();
+    gainNode.gain.value = 0.4;
+  }
 
-source.connect(gainNode);
-gainNode.connect(destination);
+  addSourse() {
+    source.connect(gainNode);
+    gainNode.connect(destination);
+  }
 
-source.start(0);
+  startSource() {
+    source.start(0);
+  }
+}
+
