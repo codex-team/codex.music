5cab8b81f009cfb22b818896472b27ba0c0874f2
Kirilllert <46005727+Kirilllert@users.noreply.github.com>
2019-10-17 00:49:37 +0300

    update filter.js
    add a new filter(delay)

--- filters.js
+++ filters.js
@@ -1,16 +1,56 @@
+/** Class create a new filter(gain). */
 class Gains {
   createNode() {
+    /**
+     * Create a node.
+     */
     const gainNode = context.createGain();
     gainNode.gain.value = 0.4;
   }
 
   addSourse() {
+    /**
+     * add a sourse.
+     */
     source.connect(gainNode);
     gainNode.connect(destination);
   }
 
-  startSource() {
+  startSourceGain() {
+    /**
+     * start a source gain.
+     */
     source.start(0);
   }
 }
+
+/** Class create a new filter(delay). */
+class Delay {
+  createDelay() {
+    /**
+     * Create a delay.
+     */
+    var delayNode = context.createDelay();
+    delayNode.delayTime.value = 2;
+  }
+
+  addSourseDelay() {
+    /**
+     * add a sourse for Delay.
+     */
+    source.connect(delayNode);
+    delayNode.connect(destination);
+  }
+
+  startSourceDelay() {
+    /**
+     * start a source delay.
+     */
+    source.start(0);
+  }
+}
+
+
+
+
 
