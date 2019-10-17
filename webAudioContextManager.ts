b4ac65fcce05ab31b84203e04e450c55a5258cd5
overkam <44203987+overkam@users.noreply.github.com>
2019-10-17 19:51:26 +0300

    add comments
    Co-Authored-By: Chechet Alexey <chechet.alexey@ya.ru>

--- webAudioContextManager.ts
+++ webAudioContextManager.ts
@@ -1,6 +1,14 @@
+/**
+ *  Class representing an AudioContextManager
+ */
 class AudioContextManager {
-	private _contex: AudioContex;
 
+	private _contex: AudioContex;
+	
+	/**
+	 *  Create AudioContext
+	 * @returns {AudioContext} _context field
+	 */
 	get context() {
 		if (this._contex == undefined) this._context = new AudioContext;
 		return this._context;
