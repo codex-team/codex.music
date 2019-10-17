a59a5704b3cf440fec44705ebb2f32e841139a5b
rogulinilya <rogulinilya@gmail.com>
2019-10-17 18:26:48 +0300

    add jsdoc

--- instruments.js
+++ instruments.js
@@ -1,28 +1,54 @@
-class Instruments extends Audio {
+/**
+ * Class representing an instrument.
+ * @extends Audio
+ */
+
+class Instrument extends Audio {
+    /**
+     * Create a instrument.
+     * @param {string} instrument - Name of the instrument.
+     */
     constructor(instrument) {
         super();
         this.instrument = instrument;
         this.setWave();
     }
 
-    setWave() {
-        let sin = [];
-        switch (this.instrument) {
+    /**
+     * Get the wave of instrument.
+     * @param {string} instrument - Name of the instrument.
+     * @returns {void}
+     */
+
+    getWave(instrument) {
+        let wave;
+        switch (instrument) {
             case 'violin':
-                sin = [0, 1, 4 / 9, 4 / 9, 4 / 9, 6 / 9, 5 / 9, 6 / 9, 5 / 9, 3 / 9, 3 / 9, 2 / 9, 1 / 9, 2 / 9, 2 / 9, 1 / 9];
+                wave = [0, 1, 4 / 9, 4 / 9, 4 / 9, 6 / 9, 5 / 9, 6 / 9, 5 / 9, 3 / 9, 3 / 9, 2 / 9, 1 / 9, 2 / 9, 2 / 9, 1 / 9];
                 break;
             case 'viola':
-                sin = [0, 3 / 6, 1, 5 / 12, 5 / 6, 2 / 6, 4 / 6, 1 / 6, 2 / 6, 1 / 12, 1 / 6];
+                wave = [0, 3 / 6, 1, 5 / 12, 5 / 6, 2 / 6, 4 / 6, 1 / 6, 2 / 6, 1 / 12, 1 / 6];
                 break;
             case 'bass':
-                sin = [0, 1, 0.8144, 0.2062, 0.0206];
+                wave = [0, 1, 0.8144, 0.2062, 0.0206];
                 break;
             case 'horn':
-                sin = [0, 0.4, 0.4, 1, 1, 1, 0.3, 0.7, 0.6, 0.5, 0.9, 0.8];
+                wave = [0, 0.4, 0.4, 1, 1, 1, 0.3, 0.7, 0.6, 0.5, 0.9, 0.8];
                 break;
         }
-        const image = new Float32Array(sin);
+        this.wave = wave;
+    }
+
+    /**
+     * Set the wave of instrument.
+     * @returns {void}
+     */
+
+    setWave() {
+        this.getWave(this.instrument);
+        const image = new Float32Array(this.wave);
         const real = new Float32Array(image.length);
         this.oscillator.setPeriodicWave(this.context.createPeriodicWave(real, image));
     }
+
 }
