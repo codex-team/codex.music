42a7587e322244986b44aadeee03aa46b05cface
ilyamore88 <themorozik@gmail.com>
2019-10-18 23:42:08 +0300

    add function for validate note

---
+++ src/utils/noteValidation.ts
@@ -0,0 +1,12 @@
+/**
+ * Function validate note
+ * @param {String} note - full note name ('A4', 'A4#x2' etc.)
+ * @return {Boolean}
+ */
+export default function (note: string) {
+  return /^[A-G][0-8]$/.test(note) || // note like 'E3'
+    /^[A-G][0-8]x\d+$/.test(note) || // note like 'G7x2'
+    /^.$/.test(note) || // note like '.'
+    /^.x\d+$/.test(note) || // note like '.x4'
+    false;
+}
