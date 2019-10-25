959c6c5b848b15c015bc30de6590e7876acf5636
ilyamore88 <themorozik@gmail.com>
2019-10-25 18:18:13 +0300

    delete node_modules

--- codex.music-audiocontext/README.md
+++
@@ -1,7 +0,0 @@
-# codex.music
-
-### install dependencies
-`yarn install --frozen-lockfile`
-
-### for local development use
-`yarn dev`
--- codex.music-audiocontext/src/index.ts
+++
@@ -1,7 +0,0 @@
-import { audioContextManager } from './webAudioContextManager';
-
-// console.log(audioContextManager.context);
-
-document.getElementById('test').onclick = () => {
-  console.log(audioContextManager.context);
-};
