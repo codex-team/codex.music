d59153d6091c07ab7aea5a38a1e20c79408164bf
ilyamore88 <themorozik@gmail.com>
2019-10-28 20:46:19 +0300

    fix indent size, use isAudioContextDefined in getAudioContext method

--- src/modules/audioContextManager.ts
+++ src/modules/audioContextManager.ts
@@ -4,35 +4,35 @@
  * If AudioContext doesn't exist, this manager will create it
  */
 class AudioContextManager {
- /**
-  * Field with page's AudioContext
-  */
- private audioContext: AudioContext;
+  /**
+   * Field with page's AudioContext
+   */
+  private audioContext: AudioContext;
 
- /**
-  * Getter for audioContext field
-  * If audioContext is empty, getter will create new audio context
-  * @return {AudioContext}
-  */
- public getAudioContext(): AudioContext {
-   if (!this.audioContext) {
-     /**
-      * Cross-browser audio context creation
-      */
-     const AudioContext = window.AudioContext || (window as any).webkitAudioContext;
+  /**
+   * Getter for audioContext field
+   * If audioContext is empty, getter will create new audio context
+   * @return {AudioContext}
+   */
+  public getAudioContext(): AudioContext {
+    if (!this.isAudioContextDefined()) {
+      /**
+       * Cross-browser audio context creation
+       */
+      const AudioContext = window.AudioContext || (window as any).webkitAudioContext;
 
-     this.audioContext = new AudioContext();
-   }
-   return this.audioContext;
- }
+      this.audioContext = new AudioContext();
+    }
+    return this.audioContext;
+  }
 
- /**
-  * Method reports if audio context is defined
-  * @return {Boolean}
-  */
- public isAudioContextDefined(): boolean {
-   return !!this.audioContext;
- }
+  /**
+   * Method reports if audio context is defined
+   * @return {Boolean}
+   */
+  public isAudioContextDefined(): boolean {
+    return !!this.audioContext;
+  }
 }
 
 export default new AudioContextManager();
