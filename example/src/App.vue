7a18105a1f65658ecdef7bc0d348285f748d72f9
ilyamore88 <themorozik@gmail.com>
2019-10-31 20:48:50 +0300

    init example app

---
+++ example/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    Demo application for Codex.Music
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  components: {}
+}
+</script>
+
+<style>
+</style>
