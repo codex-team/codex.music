2dba3423abdf7e1b2a21b6ea18acc33d841be070
rogulinilya <rogulinilya@gmail.com>
2019-11-01 20:21:32 +0300

    fix code style

--- src/modules/instrument.ts
+++ src/modules/instrument.ts
@@ -2,16 +2,16 @@
  * Class representing an instrument.
  */
 export default abstract class Instrument {
-    /**
-     * @property {string} name - Name of the instrument.
-     */
-    public name: string;
-
-    /**
-     * Create a instrument.
-     * @param {string} name - Name of the instrument.
-     */
-    constructor(name: string) {
-      this.name = name;
-    }
+  /**
+  * @property {string} name - Name of the instrument.
+  */
+  public name: string;
+    
+  /**
+  * Create a instrument.
+  * @param {string} name - Name of the instrument.
+  */
+  constructor(name: string) {
+    this.name = name;
+  }
 }
