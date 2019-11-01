8e7f6d80643d2bae33776f079acaf03301ba3fa5
kirilllyzhenkoff <kirilllyzhenkoff@gmail.com>
2019-11-01 23:34:08 +0300

    delete js files and create filter Gain in filter.ts
    delet filter.js,filters.js
    add filter.ts
    add gain filter

--- filter.ts
+++ filter.ts
@@ -16,33 +16,4 @@ class Gain extends Filter {
   }
 }
 
-class Delay extends Filter {
-  /**
-   * Create a delay and set time.
-   * Class create a new filter(delay).This module allows you to delay the sound for a certain time.
-   */
-  constructor(delayNode: any, source: any, destination: any) {
-    this.delayNode = context.createDelay();
-    delayNode.delayTime.value = 2;
-    source.connect(delayNode);
-    delayNode.connect(destination);
-    source.start(0);
-    super()
-  }
-}
-class highPass extends Filter{
-  /**
-   * high-pass filter (cuts off anything below the selected frequency)
-   */
-  constructor(highPassNode: any) {
-    this.highPassNode = context.createBiquadFilter();
-    highPassNode.type = 2;
-    highPassNode.frequency.value = 1000;
-    highPassNode.frequency.Q = 1;
-    super()
-  }
-}
-
 const gainFilter: Gain = new Gain();
-const delayFilter: Delay = new Delay();
-const highPassFilter: highPass = new highPass();
