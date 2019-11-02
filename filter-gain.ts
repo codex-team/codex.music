52369311d3ace75cab3e4e0f798d35a410645bc7
kirilllyzhenkoff <kirilllyzhenkoff@gmail.com>
2019-11-02 11:55:32 +0300

    update filter-gain.ts
    documented parameters
    create comments

--- filter-gain.ts
+++ filter-gain.ts
@@ -8,10 +8,11 @@ class Gain extends Filter {
      * @param gainNode - new gain object
      * @param source - source of melody
      */
+    super();
     this.gainNode.gain.value = 0.4; // value 0..1 (can be changed dynamically)
-    this.source.connect(gainNode);
+    this.source.connect(gainNode); // connect source of melody to object(gainNode)
     gainNode.connect(destination);
     source.start(0);
-    super();
+
   }
 }
