b32e5479f4e0a7404921e742cd31512000571e35
kirilllyzhenkoff <kirilllyzhenkoff@gmail.com>
2019-11-03 22:38:39 +0300

    build webpack and comment constructor

--- filter-gain.ts
+++ filter-gain.ts
@@ -5,15 +5,16 @@ import Filter from './filter'
  * Class create a new filter(gain).The module allows you to change the sound signal level.
  */
 class Gain extends Filter {
+  /**
+   * @param gainNode - new gain object
+   * @param source - source of melody
+   * @param destination - place of destination
+   */
   constructor(gainNode: any, source: any, destination: any) {
-    /**
-     * @param gainNode - new gain object
-     * @param source - source of melody
-     */
     super();
     this.gainNode.gain.value = 0.4; // value 0..1 (can be changed dynamically)
     this.source.connect(gainNode); // connect source of melody to object(gainNode)
-    gainNode.connect(destination);
+    gainNode.connect(destination); // connect with place of destination
     source.start(0);
   }
 }
