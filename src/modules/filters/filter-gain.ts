c7c0c91cdbac25094a43a2fdb410609eb4e9b7ab
kirilllyzhenkoff <kirilllyzhenkoff@gmail.com>
2019-11-04 20:24:51 +0300

    update JSDoc

--- src/modules/filters/filter-gain.ts
+++ src/modules/filters/filter-gain.ts
@@ -14,12 +14,12 @@ class GainFilter extends Filter {
    * Sound knot
    */
   public source: any;
-
+  /**
+   * @param gainNode - new gain object
+   * @param source - melody knot
+   * @param destination - place of purpose
+   */
   constructor(gainNode: any, source: any, destination: any) {
-    /**
-     * @param gainNode - new gain object
-     * @param source - melody knot
-     */
     super();
     this.gainNode.gain.value = 0.4; // value 0..1 (can be changed dynamically)
     this.source.connect(gainNode); // connect source of melody to object(gainNode)
