35778cedc235a4e762e9ce02bcd20d7da3f4335b
kirilllyzhenkoff <kirilllyzhenkoff@gmail.com>
2019-11-04 18:28:34 +0300

    change tab

--- src/modules/filters/filter-gain.ts
+++ src/modules/filters/filter-gain.ts
@@ -17,15 +17,13 @@ class GainFilter extends Filter {
 
   constructor(gainNode: any, source: any, destination: any) {
     /**
-    * @param gainNode - new gain object
-    * @param source - melody knot
-    */
- super();
- this.gainNode.gain.value = 0.4; // value 0..1 (can be changed dynamically)
- this.source.connect(gainNode); // connect source of melody to object(gainNode)
- gainNode.connect(destination); // connect with place of destination
- source.start(0);
- }
- }
-
- }
+     * @param gainNode - new gain object
+     * @param source - melody knot
+     */
+    super();
+    this.gainNode.gain.value = 0.4; // value 0..1 (can be changed dynamically)
+    this.source.connect(gainNode); // connect source of melody to object(gainNode)
+    gainNode.connect(destination); // connect with place of destination
+    source.start(0);
+  }
+}
