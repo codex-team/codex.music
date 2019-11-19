43bf7a0902440a2546613c593f26d07af1552350
Kirill Lyzhenkoff <46005727+Lyzhenkoff@users.noreply.github.com>
2019-11-19 06:52:07 +0300

    delete filter 2

--- src/modules/filters/filter 2.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Create class Filter.(Using filters, we create high-level, configurable, and ready-to-use modules.
- * These are amplifiers, delay lines, filters, convolution modules, splitters and mergers)
- */
-export default abstract class Filter {
-}
