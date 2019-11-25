1fcc203a58f268514a5a35f331324952513afa2f
ilyamore88 <themorozik@gmail.com>
2019-11-25 22:32:44 +0300

    rename file to PascalCase, add typing and fix docs

--- src/modules/filters/Filter.ts
+++ src/modules/filters/Filter.ts
@@ -1,4 +1,5 @@
 import audioContextManager from '../AudioContextManager';
+
 /**
  * Create class Filter.(Using filters, we create high-level, configurable, and ready-to-use modules.
  * These are amplifiers, delay lines, filters, convolution modules, splitters and mergers)
@@ -7,19 +8,20 @@ export default abstract class Filter {
   /**
    * Node of filter
    */
-  protected node: any;
+  protected node: BiquadFilterNode;
+
   /**
-   * create a new filter
+   * Create a new filter
    */
   protected constructor() {
     this.node = audioContextManager.getAudioContext().createBiquadFilter();
   }
 
   /**
-   * connect filter and instrument
-   * @param target
+   * Connect filter and instrument
+   * @param target - node for connecting filter
    */
-  connect(target) {
+  public connect(target: AudioDestinationNode): void {
     target.connect(this.node);
   }
 }
